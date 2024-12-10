abc558d5562124856911014996249fdbc21ca992
newaz10 <mh.newaz001@gmail.com>
2024-12-10 13:22:41 +0600

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+// import React from 'react'
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import arrow_icon from "../../assets/arrow_icon.png"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Feature</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+
+      <div className="nav-right">
+        <select className="nav-select">
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+            <option value="inr">INR</option>
+        </select>
+        <button className="signup-btn">Sign Up <img src={arrow_icon} alt="" /></button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
